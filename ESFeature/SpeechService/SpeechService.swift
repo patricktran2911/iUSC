3fef7edc51c25b28b5a2024e6a889e1c8f6f596a
Patrick Tran <patricktran291197@gmail.com>
2025-05-22 20:22:12 -0700

    add adjust voice speed

--- ESFeature/SpeechService/SpeechService.swift
+++ ESFeature/SpeechService/SpeechService.swift
@@ -1,14 +1,26 @@
 import AVFoundation
 
 public final class SpeechService {
+    public enum VoiceSpeed: Float {
+        case slow = 0.45
+        case medium = 0.5
+        case fast = 0.58
+    }
+    
     private let synthesizer = AVSpeechSynthesizer()
+    private var voiceSpeed: VoiceSpeed = .fast
 
     public init() {}
+    
     public func speak(_ text: String, languageCode: String = "en-US") {
         let utterance = AVSpeechUtterance(string: text)
         utterance.voice = AVSpeechSynthesisVoice(language: languageCode)
-        utterance.rate = 0.5 // adjust speaking speed
+        utterance.rate = voiceSpeed.rawValue // adjust speaking speed
         synthesizer.speak(utterance)
+    }
+    
+    public func updateVoiceSpeed(_ voiceSpeed: VoiceSpeed) {
+        self.voiceSpeed = voiceSpeed
     }
 
     public func stop() {
